c1e611230a9fde0a5fb3b3abee3c408b987ed172
ElizAnto <elizarov@anton-olegovich.ru>
2025-04-24 09:38:16 +0400

    add logger

--- GoogleMapsAPI/utils/http_methods.py
+++ GoogleMapsAPI/utils/http_methods.py
@@ -1,4 +1,5 @@
 import requests
+from utils.logger import Logger
 
 """Список HTTP методов"""
 
@@ -8,20 +9,28 @@ class HttpMethod:
 
     @staticmethod # Позволяет вызывать функцию откуда угодно
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=HttpMethod.headers, cookies=HttpMethod.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=HttpMethod.headers, cookies=HttpMethod.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="PUT")
         result = requests.put(url, json=body, headers=HttpMethod.headers, cookies=HttpMethod.cookie)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body, headers=HttpMethod.headers, cookies=HttpMethod.cookie)
+        Logger.add_response(result)
         return result
